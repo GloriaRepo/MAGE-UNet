96a79f22452fd1e97c9ead1e0fef71432518ab70
CosmoCoder <3313219659@qq.com>
2025-10-10 11:07:39 +0800

    Initial commit: MAGE-UNet - Multi-scale Attention-Guided Encoder-Decoder for Medical Image Segmentation

---
+++ networks/vit_seg_modeling_resnet_skip.py
@@ -0,0 +1,300 @@
+import math
+
+from os.path import join as pjoin
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def np2th(weights, conv=False):
+    if conv:
+        weights = weights.transpose([3, 2, 0, 1])
+    return torch.from_numpy(weights)
+
+
+class StdConv2d(nn.Conv2d):
+
+    def forward(self, x):
+        w = self.weight
+        v, m = torch.var_mean(w, dim=[1, 2, 3], keepdim=True, unbiased=False)
+        w = (w - m) / torch.sqrt(v + 1e-5)
+        return F.conv2d(x, w, self.bias, self.stride, self.padding,
+                        self.dilation, self.groups)
+
+
+def conv3x3(cin, cout, stride=1, groups=1, bias=False):
+    return StdConv2d(cin, cout, kernel_size=3, stride=stride,
+                     padding=1, bias=bias, groups=groups)
+
+
+def conv1x1(cin, cout, stride=1, bias=False):
+    return StdConv2d(cin, cout, kernel_size=1, stride=stride,
+                     padding=0, bias=bias)
+
+
+class PreActBottleneck(nn.Module):
+    def __init__(self, cin, cout=None, cmid=None, stride=1):
+        super().__init__()
+        cout = cout or cin
+        cmid = cmid or cout//4
+
+        self.gn1 = nn.GroupNorm(32, cmid, eps=1e-6)
+        self.conv1 = conv1x1(cin, cmid, bias=False)
+        self.gn2 = nn.GroupNorm(32, cmid, eps=1e-6)
+        self.conv2 = conv3x3(cmid, cmid, stride, bias=False)
+        self.gn3 = nn.GroupNorm(32, cout, eps=1e-6)
+        self.conv3 = conv1x1(cmid, cout, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+
+        if (stride != 1 or cin != cout):
+            self.downsample = conv1x1(cin, cout, stride, bias=False)
+            self.gn_proj = nn.GroupNorm(cout, cout)
+
+    def forward(self, x):
+        residual = x
+        if hasattr(self, 'downsample'):
+            residual = self.downsample(x)
+            residual = self.gn_proj(residual)
+
+        y = self.relu(self.gn1(self.conv1(x)))
+        y = self.relu(self.gn2(self.conv2(y)))
+        y = self.gn3(self.conv3(y))
+
+        y = self.relu(residual + y)
+        return y
+
+    def load_from(self, weights, n_block, n_unit):
+        conv1_weight = np2th(weights[pjoin(n_block, n_unit, "conv1/kernel")], conv=True)
+        conv2_weight = np2th(weights[pjoin(n_block, n_unit, "conv2/kernel")], conv=True)
+        conv3_weight = np2th(weights[pjoin(n_block, n_unit, "conv3/kernel")], conv=True)
+
+        gn1_weight = np2th(weights[pjoin(n_block, n_unit, "gn1/scale")])
+        gn1_bias = np2th(weights[pjoin(n_block, n_unit, "gn1/bias")])
+
+        gn2_weight = np2th(weights[pjoin(n_block, n_unit, "gn2/scale")])
+        gn2_bias = np2th(weights[pjoin(n_block, n_unit, "gn2/bias")])
+
+        gn3_weight = np2th(weights[pjoin(n_block, n_unit, "gn3/scale")])
+        gn3_bias = np2th(weights[pjoin(n_block, n_unit, "gn3/bias")])
+
+        self.conv1.weight.copy_(conv1_weight)
+        self.conv2.weight.copy_(conv2_weight)
+        self.conv3.weight.copy_(conv3_weight)
+
+        self.gn1.weight.copy_(gn1_weight.view(-1))
+        self.gn1.bias.copy_(gn1_bias.view(-1))
+
+        self.gn2.weight.copy_(gn2_weight.view(-1))
+        self.gn2.bias.copy_(gn2_bias.view(-1))
+
+        self.gn3.weight.copy_(gn3_weight.view(-1))
+        self.gn3.bias.copy_(gn3_bias.view(-1))
+
+        if hasattr(self, 'downsample'):
+            proj_conv_weight = np2th(weights[pjoin(n_block, n_unit, "conv_proj/kernel")], conv=True)
+            proj_gn_weight = np2th(weights[pjoin(n_block, n_unit, "gn_proj/scale")])
+            proj_gn_bias = np2th(weights[pjoin(n_block, n_unit, "gn_proj/bias")])
+
+            self.downsample.weight.copy_(proj_conv_weight)
+            self.gn_proj.weight.copy_(proj_gn_weight.view(-1))
+            self.gn_proj.bias.copy_(proj_gn_bias.view(-1))
+
+class ResNetV2(nn.Module):
+    def __init__(self, block_units, width_factor):
+        super().__init__()
+        width = int(64 * width_factor)
+        self.width = width
+
+        self.root = nn.Sequential(OrderedDict([
+            ('conv', StdConv2d(3, width, kernel_size=7, stride=2, bias=False, padding=3)),
+            ('gn', nn.GroupNorm(32, width, eps=1e-6)),
+            ('relu', nn.ReLU(inplace=True)),
+        ]))
+
+        self.body = nn.Sequential(OrderedDict([
+            ('block1', nn.Sequential(OrderedDict(
+                [('unit1', PreActBottleneck(cin=width, cout=width*4, cmid=width))] +
+                [(f'unit{i:d}', PreActBottleneck(cin=width*4, cout=width*4, cmid=width)) for i in range(2, block_units[0] + 1)],
+                ))),
+            ('block2', nn.Sequential(OrderedDict(
+                [('unit1', PreActBottleneck(cin=width*4, cout=width*8, cmid=width*2, stride=2))] +
+                [(f'unit{i:d}', PreActBottleneck(cin=width*8, cout=width*8, cmid=width*2)) for i in range(2, block_units[1] + 1)],
+                ))),
+            ('block3', nn.Sequential(OrderedDict(
+                [('unit1', PreActBottleneck(cin=width*8, cout=width*16, cmid=width*4, stride=2))] +
+                [(f'unit{i:d}', PreActBottleneck(cin=width*16, cout=width*16, cmid=width*4)) for i in range(2, block_units[2] + 1)],
+                ))),
+        ]))
+
+    def forward(self, x):
+        features = []
+        b, c, in_size, _ = x.size()
+        x = self.root(x)
+        features.append(x)
+        x = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)(x)
+        for i in range(len(self.body)-1):
+            x = self.body[i](x)
+            right_size = int(in_size / 4 / (i+1))
+            if x.size()[2] != right_size:
+                pad = right_size - x.size()[2]
+                assert pad < 3 and pad > 0, "x {} should {}".format(x.size(), right_size)
+                feat = torch.zeros((b, x.size()[1], right_size, right_size), device=x.device)
+                feat[:, :, 0:x.size()[2], 0:x.size()[3]] = x[:]
+            else:
+                feat = x
+            features.append(feat)
+        x = self.body[-1](x)
+        return x, features[::-1]
+
+
+class GlobalExtraction(nn.Module):
+    def __init__(self, dim=None):
+        super().__init__()
+        self.avgpool = self.globalavgchannelpool
+        self.maxpool = self.globalmaxchannelpool
+        self.proj = nn.Sequential(
+            nn.Conv2d(2, 1, kernel_size=1, stride=1),
+            nn.BatchNorm2d(1)
+        )
+
+    def globalavgchannelpool(self, x):
+        return x.mean(1, keepdim=True)
+
+    def globalmaxchannelpool(self, x):
+        return x.max(dim=1, keepdim=True)[0]
+
+    def forward(self, x):
+        x_clone = x.clone()
+        x_avg = self.avgpool(x)
+        x_max = self.maxpool(x_clone)
+        cat = torch.cat((x_avg, x_max), dim=1)
+        proj = self.proj(cat)
+        return proj
+
+class ContextExtraction(nn.Module):
+    def __init__(self, dim, reduction=None):
+        super().__init__()
+        self.reduction = 1 if reduction is None else 2
+        self.dconv = self.DepthWiseConv2dx2(dim)
+        self.proj = self.Proj(dim)
+
+    def DepthWiseConv2dx2(self, dim):
+        dconv = nn.Sequential(
+            nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=3, padding=1, groups=dim),
+            nn.BatchNorm2d(dim),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=3, padding=2, dilation=2),
+            nn.BatchNorm2d(dim),
+            nn.ReLU(inplace=True)
+        )
+        return dconv
+
+    def Proj(self, dim):
+        proj = nn.Sequential(
+            nn.Conv2d(in_channels=dim, out_channels=dim // self.reduction, kernel_size=1),
+            nn.BatchNorm2d(dim // self.reduction)
+        )
+        return proj
+
+    def forward(self, x):
+        x = self.dconv(x)
+        x = self.proj(x)
+        return x
+
+class MultiscaleFusion(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.local = ContextExtraction(dim)
+        self.global_ = GlobalExtraction()
+        self.bn = nn.BatchNorm2d(dim)
+
+    def forward(self, x, g):
+        x_local = self.local(x)
+        g_global = self.global_(g)
+        fuse = self.bn(x_local + g_global)
+        return fuse
+
+class MultiScaleGatedAttn(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.multi = MultiscaleFusion(dim)
+        self.selection = nn.Conv2d(dim, 2, 1)
+        self.proj = nn.Conv2d(dim, dim, 1)
+        self.bn = nn.BatchNorm2d(dim)
+        self.bn_2 = nn.BatchNorm2d(dim)
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=1, stride=1)
+        )
+
+    def forward(self, x, g):
+        x_orig = x.clone()
+        g_orig = g.clone()
+
+        multi = self.multi(x, g)
+        multi = self.selection(multi)
+        attention_weights = F.softmax(multi, dim=1)
+        A, B = attention_weights.split(1, dim=1)
+        x_att = A.expand_as(x_orig) * x_orig
+        g_att = B.expand_as(g_orig) * g_orig
+        x_att = x_att + x_orig
+        g_att = g_att + g_orig
+        x_sig = torch.sigmoid(x_att)
+        g_att_mod = x_sig * g_att
+        g_sig = torch.sigmoid(g_att)
+        x_att_mod = g_sig * x_att
+        interaction = x_att_mod * g_att_mod
+        projected = torch.sigmoid(self.bn(self.proj(interaction)))
+        weighted = projected * x_orig
+        y = self.conv_block(weighted)
+        y = self.bn_2(y)
+        return y
+
+def act_layer(act, inplace=False, neg_slope=0.2, n_prelu=1):
+    act = act.lower()
+    if act == 'relu':
+        layer = nn.ReLU(inplace)
+    elif act == 'relu6':
+        layer = nn.ReLU6(inplace)
+    elif act == 'leakyrelu':
+        layer = nn.LeakyReLU(neg_slope, inplace)
+    elif act == 'prelu':
+        layer = nn.PReLU(num_parameters=n_prelu, init=neg_slope)
+    elif act == 'gelu':
+        layer = nn.GELU()
+    elif act == 'hswish':
+        layer = nn.Hardswish(inplace)
+    else:
+        raise NotImplementedError('activation layer [%s] is not found' % act)
+    return layer
+
+class LGAG(nn.Module):
+    def __init__(self, F_g, F_l, F_int=16, kernel_size=3, groups=1, activation='relu'):
+        super(LGAG, self).__init__()
+
+        if kernel_size == 1:
+            groups = 1
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=kernel_size, stride=1, padding=kernel_size // 2, groups=groups,
+                      bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=kernel_size, stride=1, padding=kernel_size // 2, groups=groups,
+                      bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+        self.activation = act_layer(activation, inplace=True)
+
+    def forward(self, g, x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.activation(g1 + x1)
+        psi = self.psi(psi)
+        return x * psi
